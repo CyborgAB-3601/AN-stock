0eb1318f2cea3c703a9eb894cc9bade72df00aa7
CyborgAB-3601 <102860184+CyborgAB-3601@users.noreply.github.com>
2023-05-26 19:18:35 +0530

    Create home.py

---
+++ home.py
@@ -0,0 +1,11 @@
+root.title('Home|AN Stock Management')
+root.resizable(False,False)
+
+datel=Label(root,font=('times new roman',18,'bold'))
+datel.place(x=5,y=5)
+clock()
+s='Stock Management system'
+sliderl=Label(root,text=s,font=('imapct',28,'bold'))
+sliderl.place(x=200,y=0)
+
+root.mainloop
